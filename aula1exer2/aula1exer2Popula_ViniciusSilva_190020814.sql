ef5b8d7f3722dd116ad532bab192e050dc6f0e4c
vnsrz <mines@craft>
2024-03-23 22:59:37 -0300

    aula1exer2

---
+++ aula1exer2/aula1exer2Popula_ViniciusSilva_190020814.sql
@@ -0,0 +1,42 @@
+INSERT INTO PESSOA VALUES
+    (00000000000, 'Ana', 'senha1'),
+    (11111111111, 'Jorge', 'senha2'),
+    (22222222222, 'Gustavo', 'senha3'),
+    (33333333333, 'Claudio', 'senha4'),
+    (44444444444, 'Felipe', 'senha5'),
+    (55555555555, 'Mariana', 'senha6');
+
+INSERT INTO EMPREGADO VALUES
+    (1901111111, 11111111, 'Casa 1', 61932164675, 00000000000),
+    (2001111111, 11111112, 'Casa 2', 61965785838, 11111111111),
+    (2101111111, 11111113, 'Casa 3', 61935846892, 22222222222);
+
+INSERT INTO GERENTE VALUES
+    ('claudio@mail.com', 33333333333, 'Ensino medio'),
+    ('felipe@mail.com', 44444444444, 'Ensino superior'),
+    ('mariana@mail.com', 55555555555, 'Ensino superior');
+
+INSERT INTO SETOR VALUES
+    (1, 'claudio@mail.com'),
+    (2, 'felipe@mail.com'),
+    (3, 'mariana@mail.com');
+
+INSERT INTO trabalha VALUES
+    (3, 1901111111),
+    (2, 2001111111),
+    (1, 2101111111);
+
+INSERT INTO PRODUTO VALUES
+    (10, 150.00),
+    (11, 80.50),
+    (12, 55.05);
+
+INSERT INTO VENDA VALUES
+    (20, 1901111111, 150.00, '2020-06-10'),
+    (21, 2001111111, 161.00, '2021-05-11'),
+    (22, 2101111111, 55.05, '2022-03-12');
+
+INSERT INTO contem VALUES
+    (10, 20, 1),
+    (11, 21, 2),
+    (12, 22, 1);
