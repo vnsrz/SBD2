5fa182fef14f0f14e4ceac13f4febe49d703c5e8
vnsrz <fowardshift@gmail.com>
2024-07-03 19:08:11 -0300

    trabalho final

---
+++ TF_I11_DeboraMoreira_fim/TF_I11_DeboraMoreira_fimApaga.sql
@@ -0,0 +1,8 @@
+USE employees;
+
+DROP TABLE IF EXISTS dept_emp,
+                     dept_manager,
+                     titles,
+                     salaries, 
+                     employees, 
+                     departments;
